f7e2cc839837f1d4c26c4ca9f4fe837d6fc31f11
alexisjea <alexis.jeandenans@outlook.fr>
2022-02-15 21:32:38 +0100

    ledoo

---
+++ src/main/java/com/alex/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.alex.springboot.repository;
+
+import com.alex.springboot.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
